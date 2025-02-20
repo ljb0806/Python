ab673dd1d1182f4f012a848d3f3e2d4db8f1542a
Li Jiabin <146857056+ljb0806@users.noreply.github.com>
2025-02-21 00:02:10 +0800

    Delete FFT_demo directory

--- FFT_demo/simulation/modelsim/FFT_demo_iputf_input/synopsys/vcs/vcs_setup.sh
+++
@@ -1,6 +0,0 @@
-cp -f D:/datas/project/Quartus_project/FPGA_modules/FFT_demo/fft/simulation/submodules/fft_fft_ii_0_1n1024cos.hex ./
-cp -f D:/datas/project/Quartus_project/FPGA_modules/FFT_demo/fft/simulation/submodules/fft_fft_ii_0_1n1024sin.hex ./
-cp -f D:/datas/project/Quartus_project/FPGA_modules/FFT_demo/fft/simulation/submodules/fft_fft_ii_0_2n1024cos.hex ./
-cp -f D:/datas/project/Quartus_project/FPGA_modules/FFT_demo/fft/simulation/submodules/fft_fft_ii_0_2n1024sin.hex ./
-cp -f D:/datas/project/Quartus_project/FPGA_modules/FFT_demo/fft/simulation/submodules/fft_fft_ii_0_3n1024cos.hex ./
-cp -f D:/datas/project/Quartus_project/FPGA_modules/FFT_demo/fft/simulation/submodules/fft_fft_ii_0_3n1024sin.hex ./
